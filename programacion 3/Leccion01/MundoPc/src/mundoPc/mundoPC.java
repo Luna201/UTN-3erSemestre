eead6d972a275288741b0f23951b587d1de1717a
Andres Luna <andresluna201@hotmail.com>
2024-04-09 19:55:17 -0300

    añadi todos los videos mas el ejercicio de la clase 1 de Programacion3

--- programacion 3/Leccion01/MundoPc/src/mundoPc/mundoPC.java
+++ programacion 3/Leccion01/MundoPc/src/mundoPc/mundoPC.java
@@ -9,10 +9,32 @@ public class mundoPC {
         Raton ratonHP= new Raton("Bluetooth", "HP");
         Computadora computadoraHP= new Computadora("Computadora HP", monitorHP, tecladoHP, ratonHP);
         
-        Monitor monitorGamer= new Monitor("HP", 32);   //importar la clase
+        Monitor monitorGamer= new Monitor("LG", 32);   //importar la clase
         Teclado tecladoGamer= new Teclado("Bluetooth", "Gamer");
         Raton ratonGamer= new Raton("Bluetooth", "Gamer");
         Computadora computadoraGamer= new Computadora("Computadora Gamer", monitorGamer, tecladoGamer, ratonGamer);
+        Orden orden1= new Orden();  //Inicializamos el arreglo vacio
+        
+        Orden orden2= new Orden();  //Una nueva lista para el objeto orden2
+        Orden orden3= new Orden();  //Una nueva lista para el objeto orden3
+        Orden orden4= new Orden();  //Una nueva lista para el objeto orden4
+        
+        orden1.agregarComputadora(computadoraHP);
+        orden1.agregarComputadora(computadoraGamer);
+        
+        Computadora computadoraVarias= new Computadora("Computadoras de diferentes marcas",monitorHP, tecladoGamer, ratonHP);
+        orden2.agregarComputadora(computadoraVarias);
+        
+        Computadora computadoraVarias1= new Computadora("Equipo variado 1",monitorHP, tecladoGamer, ratonHP);
+        orden3.agregarComputadora(computadoraVarias1);
+        
+        Computadora computadoraVarias2= new Computadora("Equipo variado 2",monitorGamer, tecladoHP, ratonGamer);
+        orden4.agregarComputadora(computadoraVarias2);
+        
+        orden1.mostrarOrden();
+        orden2.mostrarOrden();
+        orden3.mostrarOrden();
+        orden4.mostrarOrden();
     }
    
 }
