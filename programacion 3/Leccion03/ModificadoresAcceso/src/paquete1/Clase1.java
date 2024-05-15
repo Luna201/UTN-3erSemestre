76f210a210054116f842558349cac6c25b67c293
Andres Luna <andresluna201@hotmail.com>
2024-05-15 20:20:14 -0300

    añadi video 7 de clase 3 de java

--- programacion 3/Leccion03/ModificadoresAcceso/src/paquete1/Clase1.java
+++ programacion 3/Leccion03/ModificadoresAcceso/src/paquete1/Clase1.java
@@ -1,12 +1,21 @@
 package paquete1;
 public class Clase1 {   //la clase es publica, puede usarse en cualquier clase externa o paquete
     public String atributoPublic= "Valor atributo public";
+    protected String atributoProtected = "valor atributo protected";
     
     public Clase1(){
         System.out.println("Constructor public");
     }
     
+    protected Clase1(String atributoPublic){
+        System.out.println("Constructor protected");
+    }
+    
     public void metodoPublico(){
         System.out.println("Método public");
+    }
+    
+    protected void metodoProtected(){
+        System.out.println("Método protected");
     }
 }
