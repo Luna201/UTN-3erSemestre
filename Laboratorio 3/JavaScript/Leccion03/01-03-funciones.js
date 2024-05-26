91cbb089acdfdcce667a3610a51cb3b4abc8d72f
Andres Luna <andresluna201@hotmail.com>
2024-05-26 13:38:37 -0300

    añadi videos 1 - 5 de JS, clase 3

---
+++ Laboratorio 3/JavaScript/Leccion03/01-03-funciones.js
@@ -0,0 +1,38 @@
+//FUNCION= codigo reutilizable, primero se debe declarar. Una funcion puede o no, tener parametros dentro de los parentesis, que
+//al ser llamadas seran argumentos. Dentro de la llave esta el CUERPO de la funcion
+
+miFuncion(8,2);     //Hosting: se puede llamar la funcion antes de definirla
+
+function miFuncion(a,b){
+    //console.log("Sumamos: "+(a+b));
+    return a + b;
+}
+
+//Llamamos la función 
+miFuncion(5, 4);
+
+let resultado = miFuncion(6, 7);       //creamos una variable llamada resultado
+console.log(resultado);
+
+//Declaramos una función de tipo expresión o anónima. Necesita cierre con ;
+let x = function(a, b){return a + b};       
+resultado= x(5, 6);     //al llamarla se pone la variable y parentesis
+console.log(resultado);
+
+
+//Funciones de tipo Self y Invoking. NO se puede reutilizar
+(function(a, b){
+    console.log("Ejecutando la función: "+ (a+b))
+})(9, 6);
+
+
+//una funcion puede ser un objeto, con sus propiedades y metodos. Propiedad ARGUMENTS, NO se puede usar fuera de la funcion
+console.log(typeof miFuncion);
+function miFuncionDos(a,b){
+    console.log(arguments.length);
+}
+miFuncionDos(5, 7, 3, 6);
+
+//To String
+var miFuncionTexto= miFuncionDos.toString();
+console.log(miFuncionTexto);
