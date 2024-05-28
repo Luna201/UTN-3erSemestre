39f170f19c9bdc7a3cd5d3d8e94f12994d549936
Andres Luna <andresluna201@hotmail.com>
2024-05-28 00:50:15 -0300

    añadi imagenes gif

--- programacion 3/Practicas/avatar/js/avatar.js
+++ programacion 3/Practicas/avatar/js/avatar.js
@@ -1,8 +1,14 @@
 document.getElementById('boton-personaje').addEventListener('click', () => {
     const personajeSeleccionado = document.querySelector('input[name="personaje"]:checked');
-    if (personajeSeleccionado) {
-        alert(`Has seleccionado a ${personajeSeleccionado.id}`);
-    } else {
+    if (personajeSeleccionado == aang) {
+        alert("Has seleccionado a Aang " );
+        var ventanaEmergente = window.open("", "_blank", "width=200,height=200");
+        ventanaEmergente.document.write("<img src='/image/aang.jpg' alt='Imagen'>");
+    } 
+    if(personajeSeleccionado.id){
+
+    }
+    else {
         alert('Por favor, selecciona un personaje.');
     }
 });
