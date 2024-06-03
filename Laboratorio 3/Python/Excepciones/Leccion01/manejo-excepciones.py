8d73362a9849a36c49794a30a091bc69feab0b8b
Andres Luna <andresluna201@hotmail.com>
2024-06-03 17:47:17 -0300

    manejo de excepciones, java

--- Laboratorio 3/Python/Excepciones/Leccion01/manejo-excepciones.py
+++ Laboratorio 3/Python/Excepciones/Leccion01/manejo-excepciones.py
@@ -1,10 +1,14 @@
 resultado= None
-a= "10"
-b= 0
+
 try:
+    a = int(input("Digite el primer número: "))
+    b = int(input("Digite el segundo número: "))
     resultado= a/b
-except Exception as e:
-# O except ZeroDivisionError as e:  #clase hija de la clase exception, es mas especifica, pero es mejor usar metodos genericos
-    print(f"Ocurrio un error: {e}")
+except TypeError as e:  #error especifico de tipeo
+    print(f"TypeError - Ocurrio un error: {type(e)}")
+except ZeroDivisionError as e:    #clase hija de la clase exception, es mas especifica, error de division con 0
+    print(f"ZeroDivisionError - Ocurrio un error: {e}")
+except Exception as e:      #  es un metodo generico. La clase padre va al final
+    print(f"Exception - Ocurrio un error: {e}")
 print(f"El resutado es: {resultado}")
 print("seguimos....")
