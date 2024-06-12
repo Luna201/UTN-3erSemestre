6a19d547469b40835330cce27e1e9124368ede3b
Andres Luna <andresluna201@hotmail.com>
2024-06-12 17:19:57 -0300

    1

---
+++ Laboratorio 3/Python/clase8/01_personas.sql
@@ -0,0 +1,6 @@
+
+
+--SELECT * FROM persona WHERE id_persona in (1,2,3) 
+--  #2- se usa para comentar#
+--INSERT INTO persona(nombre, apellido, email)VALUES ('Susana', 'Lara', 'slara@gmail.com') #añade info#
+--SELECT * FROM persona
