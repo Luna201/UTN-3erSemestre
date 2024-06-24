a8def99cb9fb1c751482163c5caf5b95ef59ea08
Andres Luna <andresluna201@hotmail.com>
2024-06-24 19:56:14 -0300

    añadi video 3 al 8 de JS, clase 5 completa

--- Laboratorio 3/JavaScript/Leccion04/01-04-objetos.js
+++ Laboratorio 3/JavaScript/Leccion04/01-04-objetos.js
@@ -1,3 +1,5 @@
+const { fetchSignInMethodsForEmail, applyActionCode } = require("firebase/auth");
+
 let x= 10;      //Variable de tipo primitiva
 console.log(x.length);
 console.log('Tipos primitivos');
@@ -90,11 +92,83 @@ function Persona3(nombre= 'Luis', apellido, email){     //funcion constructor. P
     this.nombre= nombre;
     this.apellido= apellido;
     this.email= email;
+    this.nombreCompleto = function(){
+        return this.nombre+' '+this.apellido;
+    }
 }
 let padre= new Persona3('Leo', 'Lopez', 'lopez@gmail.com');     //crea un nuevo objeto
 padre.nombre= "Luis"        //es posible modificar
+padre.telefono = '3983648';
 console.log(padre);
+console.log(padre.nombreCompleto());        //Utilizamos la función
 
 let madre= new Persona3('Laura', 'Contrera', 'contrera@gmail.com');
 console.log(madre)
+console.log(madre.telefono); //La propiedad no esta definida
+console.log(madre.nombreCompleto())
 
+
+//Diferentes formas de crear objetos
+//Caso objeto 1:
+let miObjeto = new Object();        // Esta es una opción formal
+
+//Caso objeto 2:
+let miObjeto2 = {};         //Esta opción es breve y recomendada
+
+//Caso String 1:
+let miCadena1 = new String('Hola');     // Sintaxis formal
+
+//Caso String 2:
+let miCadena2 = 'Hola';     // Esta es la sintaxis simplificada y recomendada
+
+//Caso con números 1
+let miNumero1 = new Number(1);      //Formal, no recomendable
+
+//Caso con números 2
+let miNumero2 = 1; //Sintaxis recomendada
+
+//Caso boleano 1
+let miBolean1 = new Boolean(false);         //Formal
+
+//Caso boleano 2
+let miBolean2 = false;      //Sintaxis recomendada
+
+//Caso arreglos 1
+let miArreglo1 = new Array();       //Formal
+
+//Caso arreglos 2
+let miArreglo2 = [];        //Sintaxis recomendada
+
+//Caso function 1
+let miFunction1 =  new function(){};        //Todo despues de new es considerado objeto
+
+//Caso function 2
+let miFunction2 = function(){};     // Notación simplificada y recomendad
+
+//Uso de prototype      modifica a persona3 y por ende a objeto padre y madre
+Persona3.prototype.telefono = '02457095874980';
+console.log(padre);
+console.log(madre.telefono);
+madre.telefono = '0274589345'       //modificación del nro de telefono objeto madre
+
+//Uso de CALL: utiliza un método de otro objeto
+let persona4 = {
+    nombre: 'Juan',
+    apellido: 'Perez',
+    nombreCompleto2: function(titulo, telefono){
+        return titulo+': '+this.nombre+ ' '+this.apellido+' '+telefono;
+        //return this.nombre+' '+this.apellido;
+    }
+}
+
+let persona5 ={
+    nombre: 'Carlos',
+    apellido: 'Lara'
+}
+console.log(persona4.nombreCompleto2('Lic.', '9245387690'));
+console.log(persona4.nombreCompleto2.call(persona5, ' Ing.', '4237865934'));
+
+
+//Método APPLY: Lee el arreglo y lo asigna como argumentos. a diferencia de call se debe crear un arreglo
+let arreglo = ['Ing.', '984235989']
+console.log(persona4.nombreCompleto2.apply(persona5, arreglo)); 
