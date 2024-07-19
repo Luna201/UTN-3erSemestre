b2b5718bedd3f2ae7012523929b05a97f932aba1
Andres Luna <andresluna201@hotmail.com>
2024-07-19 19:05:57 -0300

    añadi video 3, clase 6 de JS

--- Laboratorio 3/JavaScript/Leccion05/05-03-clases.js
+++ Laboratorio 3/JavaScript/Leccion05/05-03-clases.js
@@ -1,11 +1,17 @@
 class Persona{
     constructor(nombre, apellido){
-        this.nombre = nombre;
-        this.apellido = apellido;
+        this._nombre = nombre;
+        this._apellido = apellido;
+    }
+
+    get nombre(){
+        return this._nombre;
     }
 }
 
 let persona1 = new Persona('Martin', 'Perez');
-console.log(persona1);
+console.log(persona1.nombre)
+//console.log(persona1);
 let persona2 = new Persona('Carlos', 'Lara');
-console.log(persona2);
+console.log(persona2.nombre)
+//console.log(persona2);
