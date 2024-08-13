272123697e7c5922ace2a84b1fcf04e2d5fc2a2e
Andres Luna <andresluna201@hotmail.com>
2024-08-12 22:45:47 -0300

    complete clase 6 de JS

--- Laboratorio 3/JavaScript/Leccion05/05-03-clases.js
+++ Laboratorio 3/JavaScript/Leccion05/05-03-clases.js
@@ -1,4 +1,7 @@
-class Persona{
+// let persona3= new Persona('Carla', 'Ponce');  NO es posible declarar un objeto antes de la clase con hoisting
+//primero se crea la clase y despues los objetos, con constructor y objetos
+
+class Persona{  //Clase padre
     constructor(nombre, apellido){
         this._nombre = nombre;
         this._apellido = apellido;
@@ -10,16 +13,46 @@ class Persona{
     set nombre(nombre){
         this._nombre= nombre;
     }
+
+    get apellido(){
+        return this._apellido;
+    }
+    set apellido(apellido){
+        this._apellido= apellido;
+    }
+
+}
+
+class Empleado extends Persona{     //Clase hija
+    constructor(nombre, apellido, departamento){        //van todos los parametros, de clase padre e hija
+        super(nombre, apellido);        //se debe colocar super para obtener el constructor de la clase padre
+        this._departamento= departamento;
+    }
+
+    get departamento(){     //OBTENER
+        return this_departamento;
+    }
+    set departamento(departamento){
+        this._departamento= departamento;
+    }
 }
 
 let persona1 = new Persona('Martin', 'Perez');
 console.log(persona1.nombre)
 persona1.nombre= 'Juan Carlos';
-console.log(persona1.nombre)
+persona1.apellido= 'Sanz';
+console.log(persona1)
 //console.log(persona1);
 
 let persona2 = new Persona('Carlos', 'Lara');
 console.log(persona2.nombre)
 persona2.nombre= 'Maria Laura';
-console.log(persona2.nombre)
+persona2.apellido= 'Diaz';
+console.log(persona2)
 //console.log(persona2);
+
+
+let empleado1= new Empleado('Maria', ' Gimenez', 'Sistemas');
+console.log(empleado1);
+console.log(empleado1.nombre)
+
