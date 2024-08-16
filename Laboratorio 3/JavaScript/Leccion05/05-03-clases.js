739e792c906cd5957225ced43dfd62d24f5fe2b6
Andres Luna <andresluna201@hotmail.com>
2024-08-15 22:14:15 -0300

    agregue videos 1 a 5 de clase 7 de JS

--- Laboratorio 3/JavaScript/Leccion05/05-03-clases.js
+++ Laboratorio 3/JavaScript/Leccion05/05-03-clases.js
@@ -2,9 +2,15 @@
 //primero se crea la clase y despues los objetos, con constructor y objetos
 
 class Persona{  //Clase padre
+
+    static contadorPersonas= 0;   //Atributo estático. Se accede desde las CLASES
+    //email= 'Valor default email';       //Atributo NO estático. Se accede desde los OBJETOS
+
     constructor(nombre, apellido){
         this._nombre = nombre;
         this._apellido = apellido;
+        this.idPersona= ++Persona.contadorPersonas;
+        console.log('Se incrementa el contador: '+Persona.contadorObjetosPersona);
     }
 
     get nombre(){
@@ -22,13 +28,22 @@ class Persona{  //Clase padre
     }
 
     nombreCompleto(){
-        return this._nombre+' '+this._apellido;
+        return this.idPersona+' '+this._nombre+' '+this._apellido;
     }
 
     toString(){     //Regresa un String. Sobreescribe el método de la clase padre (object)
         //Se aplica el polimorfismo (Multiples formas en tiempo de ejecución)
         //El método que se ejecuta depende si es una referencia de la clase padre o hija
         return this.nombreCompleto();
+    }
+
+    static saludar(){       //un metodo static se asocia con una clase no con un objeto
+                            //solo se observa en la consola, no en quokka
+                            //se puede usar desde la clase hgija y con objetos de la clase hija
+        console.log('Saludos desde este método static');
+    }
+    static saludar2(persona){
+        console.log(persona.nombre+' '+persona.apellido);
     }
 }
 
@@ -73,3 +88,22 @@ console.log(empleado1.nombreCompleto());        //herencia de metodo para accede
 //Object.prototype.toString    :La clase Object permite trabajar con atributos y métodos como toString   
 console.log(empleado1.toString());      //ejecuta el método de la clase hija
 console.log(persona1.toString());       //ejecuta el método de la clase padre
+
+//persona1.saludar();   :NO se utiliza desde el objeto, sino desde la clase
+Persona.saludar();
+Persona.saludar2(persona1);
+
+Empleado.saludar();
+Empleado.saludar2(empleado1);
+
+//console.log(persona1.contadorObjetosPersona);
+console.log(Persona.contadorPersona);
+console.log(Empleado.contadorPersona);   //se puede usar con clases tanto padre como hija
+
+console.log(persona1.email);
+console.log(empleado1.email);
+//console.log(Persona.email)        //NO se puede acceder desde la clase
+console.log(persona1.toString());
+console.log(persona2.toString());
+console.log(empleado1.toString());
+console.log(Persona.contadorPersonas);
