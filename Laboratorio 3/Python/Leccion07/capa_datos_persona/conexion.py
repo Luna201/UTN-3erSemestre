b816e1d958b9198318c381ef0014281e0841f9c7
Andres Luna <andresluna201@hotmail.com>
2024-11-25 21:13:05 -0300

    Update conexion.py

--- Laboratorio 3/Python/Leccion07/capa_datos_persona/conexion.py
+++ Laboratorio 3/Python/Leccion07/capa_datos_persona/conexion.py
@@ -8,35 +8,16 @@ class Conexion:
     _PASSWORD = 'admin'
     _DB_PORT = '5432'
     _HOST = '127.0.0.1'
-    _conexion = None
+    _MIN_CON = 1
+    _MAX_CON = 5
 
     @classmethod
     def obtenerConexion(cls):
-        if cls._conexion is None:
-            try:
-                cls._conexion = bd.connect(host=cls._HOST,
-                                           user=cls._USERNAME,
-                                           password=cls._PASSWORD,
-                                           port=cls._DB_PORT,
-                                           database=cls._DATABASE)
-                log.debug(f'Conexión exitosa: {cls._conexion}')
-                return cls._conexion
-            except Exception as e:
-                log.error(f'Ocurrió un error al conectar a la base de datos: {e}')
-                sys.exit()
-        return cls._conexion
+        pass
 
     @classmethod
     def obtenerCursor(cls):
-        conexion = cls.obtenerConexion()
-        try:
-            cursor = conexion.cursor()
-            log.debug(f'Se abrió correctamente el cursor: {cursor}')
-            return cursor
-        except Exception as e:
-            log.error(f'Ocurrió un error al obtener el cursor: {e}')
-            sys.exit()
+        pass
 
 if __name__ == '__main__':
-    Conexion.obtenerConexion()
-    Conexion.obtenerCursor()
+    pass
